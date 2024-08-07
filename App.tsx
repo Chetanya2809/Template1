ce726a99de6df10dc256656c02819da1a72e86ee
Chetanya Jindal <chetanya.jindal@appinventiv.com>
2024-08-07 18:03:18 +0530

    .

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ function HomeScreen() {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'red',
+        backgroundColor: 'blue',
       }}>
       <Text>Home Screen</Text>
     </View>
